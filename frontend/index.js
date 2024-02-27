143d65e1842ec98d31a6b585a1c7d8b61f2652b4
jvazquez30 <josevazquez881@gmail.com>
2024-02-26 18:17:18 -0800

    New answers

--- frontend/index.js
+++ frontend/index.js
@@ -37,6 +37,12 @@ function moduleProject2() {
       row.appendChild(square)
       square.addEventListener('click', () => {
         // 👉 TASK 2 - Use a click handler to target a square 👈
+        if (!square.classList.contains("targeted")) {
+          document.querySelector(".targeted").classList.remove("targeted")
+          square.classList.add("targeted")
+        }
+
+        
       })
     }
   }
@@ -65,7 +71,38 @@ function moduleProject2() {
 
   document.addEventListener('keydown', evt => {
     // 👉 TASK 3 - Use the arrow keys to highlight a new square 👈
+    console.log()
 
+    let isUp = evt.key === keys.up
+    let isDown = evt.key === keys.down
+    let isLeft = evt.key === keys.left
+    let isRight = evt.key === keys.right
+
+    let target = document.querySelector(".targeted")
+
+    if (isUp) {
+      if (target.parentElement.previousElementSibling) {
+        let index = Array.from(target.parentElement.children).indexOf(target)
+        target.classList.remove("targeted")
+        target.parentElement.previousElementSibling.children[index].classList.add("targeted")
+      }
+    } else if (isDown) {
+      if (target.parentElement.nextElementSibling) {
+        let index = Array.from(target.parentElement.children).indexOf(target)
+        target.classList.remove("targeted")
+        target.parentElement.nextElementSibling.children[index].classList.add("targeted")
+      }
+    } else if (isLeft) {
+      if (target.previousElementSibling) {
+        target.classList.remove("targeted")
+        target.previousElementSibling.classList.add("targeted")
+      }
+    } else if (isRight) {
+      if (target.nextElementSibling) {
+        target.classList.remove("targeted")
+        target.nextElementSibling.classList.add("targeted")
+      }
+    }
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
 
     // 👉 TASK 5 - End the game 👈
